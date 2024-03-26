1aa3e5aad81040445e8de79277e2685948a187b9
YuliyaOryekhova <yuliavit7@gmail.com>
2024-03-26 13:07:54 -0400

    Updated index.js

--- src/index.js
+++ src/index.js
@@ -86,10 +86,11 @@ function displayForecast(response) {
     <div class="weather-forecast-temperatures">
     <div class="weather-forecast-temperature">
       <strong>${Math.round(day.temperature.maximum)}°</strong>
-    </div>
-    <div class="weather-forecast-temperature">${Math.round(
+    
+    <span class="weather-forecast-temperature">${Math.round(
       day.temperature.minimum
-    )}°</div>
+    )}°</span>
+  </div>
   </div>
   </div>
 `;
